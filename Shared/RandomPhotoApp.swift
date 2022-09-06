5db170f7e3d64a70d39d3ae7e0cd5f86abc01083
jhconger <jhconger@gmail.com>
2022-09-06 13:53:51 -0500

    Initial Commit

---
+++ Shared/RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoApp.swift
+//  Shared
+//
+//  Created by justin conger on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
